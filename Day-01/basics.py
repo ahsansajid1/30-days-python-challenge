eb60ae681ea956118379f394b511bdc68b3a364c
ahsansajid1 <sardarahsanofficial302@gmail.com>
2026-04-07 16:22:32 +0500

    Day 1 Done

---
+++ Day-01/basics.py
@@ -0,0 +1,62 @@
+# Comments In python
+
+'''Single Line Commnet'''
+#Hi my name is Ahsan
+
+# Multiline commnet
+
+"""Use Docstring To write multiline comments in python """  
+
+#Variables in python(Something use to store values )
+
+a = 12
+name ="Ahsan"
+print(name)
+
+'''Three case to write a variables'''
+
+name = "AhsanSajid" #Pascal case
+name = "ahsanSajid" #Camel case
+name = "ahsan_sajid" #snake case
+
+#DataTypes In Python
+'''Things that are used to define the type of data that are store in a variable'''
+
+#Integer (All neagative and postive values)
+age = 12
+print(type(a))
+
+#Float (All number that contain values in point form or p/q form )
+a = 12.56
+print(type(a))
+
+#Complex (imaginary number such as auota i )
+b=12j
+print(type(b))
+
+#Strings (Are used to store anything in python )
+name= "Ahsan"
+age= "20"
+print(type(name))
+
+#Boolean (Something True Or False)
+c = True
+d= False
+print(type(c))
+print(type(d))
+
+#UNICODE (Universal code for every letters)
+e = 65
+print(chr(e))
+f= 'A'
+print(ord(f))
+
+#INPUT OUTPUT
+
+name= str(input("Enter your name: "))
+
+print(name)
+
+age= int(input("Enter your age: "))
+
+print(f"My age is {age}")
---
+++ Day-01/strings.py
@@ -0,0 +1,20 @@
+#Strings In Python
+'''String Indexing'''
+
+intro = "Myself, Ahsan"
+print(intro[0])
+
+'''Slicing Of String'''
+intro = "Hello, Ahsan"
+print(intro[0::1])
+
+#Type Conversion
+
+a = 12
+a=str(a)
+print(type(a))
+
+a=12.2
+a=int(a)
+print(type(a))
+
